b2b2e4e64214ab450f1e33568e511fb97fde3a50
Sanjeev <sanjeev.viswanath@gmail.com>
2023-10-08 21:31:34 -0500

    added usestate functions to update objects, and arrays

--- React-app/src/App.tsx
+++ React-app/src/App.tsx
@@ -26,6 +26,44 @@ const color = "primary";
 
 function App() {
   const [alertVisible, setAlertVisibility] = useState(false);
+  const [drink, setDrink] = useState({ title: "Coca cola", price: 5 });
+  const [customer, setCustomer] = useState({
+    name: "Sanjeev",
+    age: 48,
+    address: {
+      street: "559 W Questa Trl",
+      city: "Mountain House",
+      zip: 95390,
+    },
+  });
+  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const priceOnClick = () => {
+    //If we do like this by changing just one property of the original
+    //object, nothing happens. We should always pass a new object
+    // drink.price = 10;
+    // setDrink(drink);
+    //Instead we should do the following:
+    // const newDrink = {title: "Coca cola",price: 10}
+    // setDrink(newDrink);
+    //Other way to do that is:
+    //const newDrink = { ...drink, price: 10 };
+    setDrink({ ...drink, price: 10 });
+  };
+  const addressOnClick = () => {
+    setCustomer({ ...customer, address: { ...customer.address, zip: 95391 } });
+  };
+  //Adding to an array
+  const arrayAddOnClick = () => {
+    setTags([...tags, "sad"]);
+  };
+  //Adding an intem within an array
+  const arrayUpdateOnClick = () => {
+    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+  };
+  //Deleting items within an array
+  const arrayDeleteOnClick = () => {
+    setTags(tags.filter((tag) => tag !== "sad"));
+  };
   return (
     <div>
       {alertVisible && (
@@ -58,6 +96,14 @@ function App() {
         fillColor="red"
         size={30}
       />
+      <p>{drink.price}</p>
+      <p>{customer.address.zip}</p>
+      <p>{tags.toString()}</p>
+      <button onClick={priceOnClick}>price update</button>
+      <button onClick={addressOnClick}>address update</button>
+      <button onClick={arrayAddOnClick}>Add to array</button>
+      <button onClick={arrayUpdateOnClick}>Update an array</button>
+      <button onClick={arrayDeleteOnClick}>Delete an array</button>
     </div>
   );
 }
